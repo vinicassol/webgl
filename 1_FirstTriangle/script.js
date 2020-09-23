206abc5cdf6e05d02190af940ae37a912f1a054c
Vini Cassol <vinicassol@gmail.com>
2020-09-23 08:58:47 -0300

    Add files via upload

--- 1_FirstTriangle/script.js
+++ 1_FirstTriangle/script.js
@@ -1,3 +1,4 @@
+//Estrutura básica WebGL. FOnte: webglacademy.com
 var main=function() {
 
   var CANVAS=document.getElementById("canvas");
@@ -9,7 +10,6 @@ var main=function() {
   GL = CANVAS.getContext("experimental-webgl");
 
   /*========================= SHADERS ========================= */
-  /*jshint multistr: true */
   var shader_vertex_source="\n\
   attribute vec2 position; //the position of the point\n\
   attribute vec3 color;  //the color of the point\n\
@@ -19,7 +19,6 @@ var main=function() {
   gl_Position = vec4(position, 0., 1.); //0. is the z, and 1 is w\n\
   vColor=color;\n\
   }";
-
 
   var shader_fragment_source="\n\
   precision mediump float;\n\
@@ -33,14 +32,14 @@ var main=function() {
 
 
   var get_shader=function(source, type, typeString) {
-  var shader = GL.createShader(type);
-  GL.shaderSource(shader, source);
-  GL.compileShader(shader);
-  if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
-  alert("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
-  return false;
-  }
-  return shader;
+    var shader = GL.createShader(type);
+    GL.shaderSource(shader, source);
+    GL.compileShader(shader);
+    if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+      alert("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
+      return false;
+    }
+    return shader;
   };
 
   var shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
@@ -66,11 +65,11 @@ var main=function() {
   //POINTS :
   var triangle_vertex=[
   -1,-1, //first corner: -> bottom left of the viewport
-  0,0,1,
+  0,0,1, // vermelho
   1,-1, //bottom right of the viewport
-  1,1,0,
+  1,1,0, //green
   1,1,  //top right of the viewport
-  1,0,0
+  1,0,0 //blue
   ];
 
   var TRIANGLE_VERTEX= GL.createBuffer ();
@@ -86,8 +85,6 @@ var main=function() {
   GL.bufferData(GL.ELEMENT_ARRAY_BUFFER,
   new Uint16Array(triangle_faces),
   GL.STATIC_DRAW);
-
-
 
   /*========================= DRAWING ========================= */
   GL.clearColor(0.0, 0.0, 0.0, 0.0);
